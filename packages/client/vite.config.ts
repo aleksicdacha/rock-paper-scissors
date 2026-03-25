8b7603aadfa805b41012efb17a76e22fddb00d73
Dalibor Aleksic <aleksic.dacha@gmail.com>
2026-03-25 22:39:38 +0100

    [RPS-0] Make vite proxy conditional for CI builds

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const serverUrl = env.VITE_SERVER_URL;
-  if (!serverUrl) throw new Error('Missing required env var: VITE_SERVER_URL');
   const port = parseInt(env.VITE_PORT || '5173', 10);
 
   return {
@@ -18,12 +17,9 @@ export default defineConfig(({ mode }) => {
     server: {
       host: true,
       port,
-      proxy: {
-        '/socket.io': {
-          target: serverUrl,
-          ws: true,
-        },
-      },
+      proxy: serverUrl
+        ? { '/socket.io': { target: serverUrl, ws: true } }
+        : undefined,
     },
   };
 });
